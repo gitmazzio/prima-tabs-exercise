e55a1d92bd67d6c30e9d37f05815e21d9fa623b4
Matteo Mazziotti <mmazziotti1@gmail.com>
2024-10-25 18:35:21 +0200

    feat: init project + storybook init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './globals.module.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    Hello world, this is my Prima's exercise! 😎
+  </StrictMode>,
+)
